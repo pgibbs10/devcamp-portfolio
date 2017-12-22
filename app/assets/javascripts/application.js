e1affe5b3ffed0821ceb6df0fd42a947e940c44e
Paul Gibbs <pwgibbs10@gmail.com>
2017-12-22 11:38:15 -0700

    Updated app.js with new things

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,14 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
+//= require jquery3
+//= require popper
+//= require rails-ujs
 //= require jquery-ui
 //= require html.sortable
 //= require cocoon
 //= require gritter
-//= require lib/typed
-//= require typed-implementation
 //= require cable
 //= require turbolinks
 //= require_tree .
