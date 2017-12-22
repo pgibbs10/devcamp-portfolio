ff2a75b76c9d5770e78543554f245a166fe55b4c
Paul Gibbs <pwgibbs10@gmail.com>
2017-12-22 10:34:38 -0700

    Fixed typo where it said Jordan Hudgens

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -72,7 +72,7 @@ module ApplicationHelper
   end
 
   def alert_generator msg
-    js add_gritter(msg, title: "Jordan Hudgens Portfolio", sticky: false)
+    js add_gritter(msg, title: "Paul Gibbs Portfolio", sticky: false)
   end
 
 end
